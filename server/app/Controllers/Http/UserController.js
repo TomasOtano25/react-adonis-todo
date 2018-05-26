92c1c040e3bfe3dc03fc2020c65788478370af50
Tomas Garcia <tomasotano25@gmail.com>
2018-05-26 02:21:13 -0400

    auth / register / login

---
+++ server/app/Controllers/Http/UserController.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const User = use('App/Models/User')
+
+class UserController {
+
+  async login({ request, auth }) {
+  	const { email, password } = request.all();
+  	const token = auth.attempt(email, password);
+  	return token;
+
+  }
+  async register({ request }) {
+    const { email, password } = request.all();
+    const user = await User.create({
+    	email,
+    	password,
+    	username: email
+    });
+    // console.log(email + " " + password);
+    return {
+      user
+    };
+  }
+}
+
+module.exports = UserController;

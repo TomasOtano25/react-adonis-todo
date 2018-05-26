e61a70b3cf61ca49b338b298f3e12abb55677e45
Tomas Garcia <tomasotano25@gmail.com>
2018-05-26 02:54:42 -0400

    model / project

---
+++ server/database/migrations/1527316929649_project_schema.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Schema = use("Schema");
+
+class ProjectSchema extends Schema {
+  up() {
+    this.create("projects", table => {
+      table.increments();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users");
+      table.string("title", 254);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("projects");
+  }
+}
+
+module.exports = ProjectSchema;

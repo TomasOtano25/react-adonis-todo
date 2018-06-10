754f572a0982f8ac579e7b54ab594e6b9d47e8dc
TomasOtano25 <tomasotano25@gmail.com>
2018-06-10 14:04:20 -0400

    add task model

---
+++ server/database/migrations/1528653074074_task_schema.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Schema = use("Schema");
+
+class TaskSchema extends Schema {
+  up() {
+    this.create("tasks", table => {
+      table.increments();
+      table
+        .integer("project_id")
+        .unsigned()
+        .reference("id")
+        .inTable("projects");
+      table.string("description", 510);
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("tasks");
+  }
+}
+
+module.exports = TaskSchema;
